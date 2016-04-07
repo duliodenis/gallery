bf0af6f9c7e2a8bed161163bfe39a9f9664ac83a
Dulio Denis <dulio.denis@yahoo.com>
2016-04-07 12:07:31 -0400

    Added a Collection View for the Art pieces

--- Gallery/Gallery/ViewController.swift
+++ Gallery/Gallery/ViewController.swift
@@ -8,7 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    
+    // MARK: Collection View Delegate Methods
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("ArtCollectionViewCell", forIndexPath: indexPath)
+        return cell
+    }
+    
+    
+    // MARK: Collection View Layout Delegate Methods
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.size.width - 80, height: collectionView.bounds.size.height - 40)
+    }
 }
 
---
+++ Gallery/Gallery/Views/ArtCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ArtCollectionViewCell.swift
+//  Gallery
+//
+//  Created by Dulio Denis on 4/6/16.
+//  Copyright © 2016 Dulio Denis. All rights reserved.
+//
+
+import UIKit
+
+class ArtCollectionViewCell: UICollectionViewCell {
+    
+}
